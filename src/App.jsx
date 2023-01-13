9cd5c25f90f9f452a099af79a3ce1c65f66506c9
midCenturyDude7 <mgriffes@gmail.com>
2023-01-12 22:00:39 -0800

    Programmitically added Die component 10x

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+/* eslint-disable react/jsx-key */
 import React from "react";
+import Die from "./Components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
   
+  // eslint-disable-next-line no-unused-vars
+  const [dice, setDice] = React.useState(allNewDice);
+  
+  function allNewDice() {
+    const newDice = [];
+    for(let i=0; i<10; i++) {
+      newDice.push(Math.ceil(Math.random()*6));
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map(die => 
+    <Die key={nanoid()} value={die}/>
+  );
+
   return (
-    <div>
-      <h1>Tenzies App | Built with React</h1>
-    </div>
+    <main>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+    </main>
   );
 }
 
---
+++ src/Components/Die.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+export default function Die(props) {
+
+  
+
+  return (
+    <div className="die-face">
+      <h2 className="die-num">{props.value}</h2>
+    </div>
+  );
+}
